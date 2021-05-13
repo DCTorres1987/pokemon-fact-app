dfe939619f87d83bc4d8d4cbd62e5cd228a0c913
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-05-12 22:26:26 -0400

    work on adding pokemon and fixing errors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
-// import { pokemons } from '.pokemons';
+import { pokemons } from './components/pokemons';
 
 const title = '';
 const background = <img className="background"
                         src='./images/background.jpg'
                         alt='background' />
 
+const images = [];
+
+for (const pokemon in pokemons ) {
+
+  images.push(
+    <img key={pokemon}
+         className='pokemon'
+         alt={pokemon}
+         src={pokemons[pokemon].image}
+         ariaLabel={pokemon}
+         role= 'button' />
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <h1>{title || 'Click an pokemon for a fun fact'}</h1>
       {background}
+      <div className='pokemons'>
+        {images}
+      </div>
       
     </div>
   );
--- src/components/pokemons.js
+++ src/components/pokemons.js
@@ -1,14 +1,14 @@
 export const pokemons = {
     pikachu: {
-      image: '/public/images/pikachu.png',
+      image: './images/pikachu.png',
       facts: ['Pikachu is an Electric-Type Pokemon!', 'Pikachu are mainly found in forests!', 'Pikachu is weak against Ground-Type Moves!']
     },
     charmander: {
-      image: '/public/images/charmander.jpg',
+      image: './images/charmander.jpg',
       facts: ['Charmander is an Fire-Type Pokemon!', 'Charmander has two sequential evolutions, Charmeleon and Charizard!', 'Charmander is born with a flame at the end of its tail.']
     },
     squirtal: {
-      image: '/public/images/squirtal.png',
+      image: './images/squirtal.png',
       facts: ['Squirtle is scarce in the wild, although it can be found around small ponds and lakes!', 'Starfish have no brain and no blood!', 'Squirtle can spray foamy water from its mouth with great accuracy']
     }
   };
