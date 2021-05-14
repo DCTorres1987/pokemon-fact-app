f9803fa9da53dc549c591e9eb716436c03027e11
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-05-13 21:46:46 -0400

    create eventlistner on pokemon image. to display fact when clicked no

--- src/App.js
+++ src/App.js
@@ -16,19 +16,31 @@ for (const pokemon in pokemons ) {
          alt={pokemon}
          src={pokemons[pokemon].image}
          ariaLabel={pokemon}
-         role= 'button' />
+         role= 'button'
+         onClick={displayFact} />
   )
+}
+
+function displayFact(e) {
+
+  const selectedPokemon = e.target.alt;
+  const pokemonInfo = pokemons[selectedPokemon];
+  const optionIndex = Math.floor(Math.random() * pokemonInfo.facts.length);
+  const funFact = pokemonInfo.facts[optionIndex];
+  document.getElementById('fact').innerHTML = funFact;
 }
 
 function App() {
   return (
     <div className="App">
-      <div className='pokemon-header-bar'><h1 className='pokemon-header'>{title || 'Click an pokemon for a fun fact!'}</h1></div>
+      <div className='pokemon-header-bar'>
+        <h1 className='pokemon-header'>{title || 'Click an pokemon for a fun fact!'}</h1>
+        <p id='fact'className='pokemon-fact'></p>
+      </div>
       {background}
       <div className='pokemons'>
         {images}
       </div>
-      
     </div>
   );
 }
